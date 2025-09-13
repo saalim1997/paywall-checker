33f50aaff5da1d28dcf209555d9b47253cb86a7e
saalim1997 <saalim1997@gmail.com>
2025-09-14 07:58:36 +0800

    Make Redirect URI string type

--- src/paywall_checker/auth.py
+++ src/paywall_checker/auth.py
@@ -19,7 +19,7 @@ oauth.register(
 
 @router.get("/login")
 async def login(request: Request):
-    redirect_uri = request.url_for("auth")
+    redirect_uri = str(request.url_for("auth"))
     # Force HTTPS for Google OAuth
     redirect_uri = redirect_uri.replace("http://", "https://")
     print("Redirect URI:", redirect_uri)
